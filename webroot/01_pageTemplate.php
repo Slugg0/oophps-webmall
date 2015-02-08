e4e8bb41f1cb6aa11306a0f23f96cdf0b5596df1
Sluggo <slugg0@users.noreply.github.com>
2015-02-08 02:58:53 +0100

    header and footer are moved to config.php

--- webroot/01_pageTemplate.php
+++ webroot/01_pageTemplate.php
@@ -10,6 +10,15 @@ include(__DIR__.'/config.php');
 // Do it and store it all in variables in the oophps container.
 $oophps['title'] = "Hello World";
  
+$oophps['main'] = <<<EOD
+<h1>Hej Världen</h1>
+<p>Detta är en exempelsida som visar hur Oophps ser ut och fungerar.</p>
+EOD;
+
+ 
+/**
+ * Header and footer are created in config.php
+ *//*
 $oophps['header'] = <<<EOD
 <img class='siteLogo' src='img/oophps.png' alt='oophps Logo'/>
 <div id="siteHead">
@@ -18,14 +27,10 @@ $oophps['header'] = <<<EOD
 </div>
 EOD;
  
-$oophps['main'] = <<<EOD
-<h1>Hej Världen</h1>
-<p>Detta är en exempelsida som visar hur Oophps ser ut och fungerar.</p>
-EOD;
- 
 $oophps['footer'] = <<<EOD
 <footer><span class='siteFooter'>Skapad av Stefan Ros |  | <a href='http://validator.w3.org/unicorn/check?ucn_uri=referer&amp;ucn_task=conformance'>Unicorn</a></span></footer>
 EOD;
+*/ 
  
  
 // Finally, leave it all to the rendering phase of Oophps.
